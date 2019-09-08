36f28db2a8aa21d76925065fe5b61ec18b17a59e
Exr0nStatic <exr0n.static@gmail.com>
2019-09-08 07:58:50 -0700

    https set up!

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ var i = {
     deps: {
         express: require('express'),
         fs: require('fs'),
-        moment: require('moment')
+        moment: require('moment'),
+	http: require('http'),
+	https: require('https')
     },
     config: require('./config.json'),
-    path: __dirname + "/"
+    path: __dirname + "/",
+    secrets: require("./secrets.json")
 };
 i.util = require(i.config.app.scripts_location + "utility.js")(i);
 i.app = i.deps.express();
@@ -24,4 +27,17 @@ for (var r in routes) {
     i.app.use(r, routes[r]);
 }
 
-i.app.listen(require('./secrets.json').port, () => { console.log(`Listening!`); } );
+
+i.ssl_creds = {
+	key: i.deps.fs.readFileSync('/etc/letsencrypt/live/www.exr0n.com/privkey.pem', 'utf8'),
+	cert: i.deps.fs.readFileSync('/etc/letsencrypt/live/www.exr0n.com/cert.pem', 'utf8'),
+	ca: i.deps.fs.readFileSync('/etc/letsencrypt/live/www.exr0n.com/chain.pem', 'utf8')
+}
+i.deps.servers = {
+	http: i.deps.http.createServer(i.app),
+	https: i.deps.https.createServer(i.ssl_creds, i.app)
+}
+for (let key in i.deps.servers)
+{
+	i.deps.servers[key].listen(i.secrets.ports[key], () => { console.log(`Listening on port ${i.secrets.ports[key]}`); });
+}
--- scripts/middlewares.js
+++ scripts/middlewares.js
@@ -4,10 +4,15 @@ module.exports = (ref) => {
         logger: (req, res) => {
             let text = `
 ${ref.util.timestamp()}:
-    req made!
+    ${req.method} ${req.protocol}:/\/${req.headers.host}${req.url}
+    from ${req.ip}
 `;
             ref.util.log(text);
             req.next();
-        }
+        },
+	require_secure: (req, res) => {
+		if (! req.secure) res.redirect(301, 'https://' + req.headers.host + req.url);
+		else req.next();
+	}
     }
 }
