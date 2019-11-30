5754d0c7fb2c8321f18f951cd613888ab6d73740
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-11-30 12:24:41 -0800

    freaking out over programing paradigms should I use functional programming? Everyone seems to say oop is bad...

--- scripts/templates.js
+++ scripts/templates.js
@@ -8,15 +8,24 @@ class User {
         ref.deps.fs.createWriteStream(ref.path + 'storage/data.json')
             .write(JSON.stringify(i.data, (k, v) => v, 2));
     }
+    /* utility */
     static _users = {};
     static get users()
     {
         return User._users;
     }
+    static get_user(name) { // todo: this, paused here
+        return Promise.resolve().then(() => {
+            if (this.exists(name)) {
+                return this._users[name];
+            } else throw new Error("User doesn't exist!");
+        });
+    }
     /* core interface */
     static exists(name) {
         return User._users.hasOwnProperty(name);
     }
+    /* instance properties */
     constructor() {}
     from_json(json) {
         this.json = json;
@@ -24,13 +33,35 @@ class User {
     get data() {
         return this.json;
     }
+    get public() {
+        return this.json.public;
+    }
+    get private() {
+        return this.json.private;
+    }
+    get meta() {
+        return this.json.meta;
+    }
     stringify() {
         return JSON.stringify(this.json);
     }
 }
+
+class Gaurdian {
+    constructor () {}
+    async authenticate (data) {
+        if (!data) throw new Error("wrong_input");
+        if (!data.usr || !data.pwd) throw new Error("wrong_input");
+        return User.get_user(name).then(
+            (user) => ref.deps.bcrypt.compare(data.pwd, user.meta.pwd)
+        ).catch((e) => {throw new Error(e);});
+    }
+}
+
 module.exports = (_ref) => {
     ref = _ref;
     return {
-        User: User
+        User: User,
+        Gaurdian: Gaurdian
     };
 }
